cec0d7fbcd5bdd41faf86732fb0d1f14d470ef04
luca.liu <243257686@qq.com>
2018-09-28 18:04:28 +0800

    [修改] 1.页面重复/多余代码删除

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -10,26 +10,10 @@
 				<p class="us-level">普通会员</p>
 			</div>
 		</div>
-			<h2 @click="deleteCookie()">this is user information . </h2>
+			
 	</div>
 		<div class="panel_body">
-			<!-- <label for='my_file' class="theme-color">
-			<mu-icon left value="backup"></mu-icon>
-			修改头像
-			</label>
-			<input type="file" ref="upload" name="avatar" id='my_file' style="display:none;" accept="image/jpg" @change="changeAvatar" /> -->
 			
-			
-			<form action="/api/uploadImg/postImg" method="post" enctype="multipart/form-data">
-				<div class="field clearfix">
-					<label for="uploadFile">用户头像：</label>
-					<input type="file" id="uploadFile" accept="image/*" placeholder="请上传头像" name="avatar" ref="avatar" @change="changeAvatar" >
-				</div>
-				
-				<div class="field field_btn clearfix" style="text-align: center;">
-					<a href="javascript:void(0);" class="btn btn-default btn_login2" @click="postImg">提交</a>
-				</div>
-			</form>
 			<el-upload
 				ref = 'upload'
 				class="avatar-uploader"
@@ -39,8 +23,8 @@
 				:auto-upload="false" 
 				:on-success="handleAvatarSuccess"
 				:before-upload="beforeAvatarUpload">
-				<img v-if="resImgSrc" :src="resImgSrc" class="avatar">
-				<i v-else class="el-icon-plus avatar-uploader-icon"></i>
+				
+				<i v-if="!resImgSrc" class="el-icon-plus avatar-uploader-icon"></i>
 				<el-button slot="trigger" type="primary" size="small">选取文件</el-button>
 				<el-button type="success" size="small" style="margin-left: 15px;" @click="submitUpload">上传图片</el-button>
 			</el-upload>
@@ -67,12 +51,6 @@ export default {
 			} */
 		};
 	},
-	/* watch: {
-		resImgSrc: function(newValue, oldValue){
-			console.log(this.styleObject);
-			this.styleObject.backgroundImage = "url("+ this.resImgSrc +")";
-		}
-	}, */
 	methods: {
 		handleAvatarSuccess(res, file) {
 			this.resImgSrc = URL.createObjectURL(file.raw);
@@ -104,9 +82,7 @@ export default {
 			this.delCookie(this.sessionName);
 			this.checkLogin();
 		},
-		getUserInfo() {
-			this.$store.dispatch("getUserInfo");
-		},
+		
 		changeAvatar(event) {
 			var imgs = event.target.files || event.dataTransfer.files;
 			if (!imgs.length) return; //判断图片是否为空
@@ -144,48 +120,9 @@ export default {
 					type: 'success'
 				})
 			})
-		},
-		postImg() {
-			if (this.correctImgType == false) {
-				window.alert("请选择以下正确的图片格式：.jpg,.jpeg,.png,.bmp,.gif");
-				return false;
-			}
-			var username = this.getCookie('userSession');
-			var formData = new FormData();
-			formData.append("avatar", this.avatar); //使用FormData，第一个参数为input的name，第二个参数为文件对象
-			formData.append('userSession',username);
-			this.$http
-				.post("/api/uploadImg/postImg", formData)
-				.then(res => {
-					
-					this.resImgSrc = res.body.imgSrc;
-					//this.styleObject.backgroundImage = "url("+res.body.imgSrc+");"
-					
-					//console.log(this.styleObject.backgroundImage);
-					localStorage.userface = res.body.imgSrc;
-					
-					//console.log(localStorage.userInfo);
-					window.alert(res.body.message);
-				})
-				.catch(err => {
-				window.alert(err);
-				});
-
-			//以下为vue-resource post的详细写法，同样可以用
-			/*  this.$http({
-					method: 'POST',
-					url: '/api/uploadImg/getImg',
-					headers: {
-						'Content-Type':'application/x-www-form-urlencoded;charset=utf-8'
-					},
-					body: formData
-				}).then(res=>{
-					console.log(res.body.message)
-				}) */
 		}
 	},
 	mounted() {
-		this.getUserInfo();
 		this.resImgSrc = localStorage.userface;
 		//console.log(this.resImgSrc);
 	}
@@ -194,6 +131,9 @@ export default {
 
 <style scoped>
 /* element-ui style */
+.avatar-uploader {
+	margin-top: 15px;
+}
 .avatar-uploader .el-upload {
 	border: 1px dashed #d9d9d9;
 	border-radius: 6px;
